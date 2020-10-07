b44075e7d40ab8214ab46cd2b7178ed2504f20e7
Rachel <ColourfulDemon@Rachels-MacBook-Air.local>
2020-10-07 19:21:17 +1300

    Enhanced style

--- paddle.c
+++ paddle.c
@@ -1,3 +1,10 @@
+/**
+    @file paddle.c
+    @author Matthew Bond, Rachel Hodgson
+    @date 07 October 2020
+    @brief Paddle module, contains movement functions for the paddle
+ 
+ */
 
 #include "system.h"
 #include "navswitch.h"
@@ -6,15 +13,13 @@
 #include "pio.h"
 #include "../fonts/font3x5_1.h"
 
-
-
-
+/* Define the paddle as a structure with the 3 relevant position aspects */
 typedef struct paddle_s Paddle_str;
 struct paddle_s {
 	tinygl_point_t pos, pos_2, pos_3;
 }paddle;
 
-
+/* Initialise the paddle positions as individual points */
 Paddle_str paddle_initial(void) {
 	
 
@@ -27,9 +32,8 @@ Paddle_str paddle_initial(void) {
 	return paddle;
 }
 
+/* Light up the initial paddle on the LED matrix */
 void paddle_initial_points(void) {
-	
-	
 	
 	tinygl_draw_point (paddle.pos, 1);
 	tinygl_draw_point (paddle.pos_2, 1);
@@ -38,8 +42,7 @@ void paddle_initial_points(void) {
     
 }
 
-
-
+/* Add a new paddle point */
 void point_add (tinygl_point_t pos, int direction) {
      
     tinygl_point_t prev_pos;
@@ -54,7 +57,7 @@ void point_add (tinygl_point_t pos, int direction) {
     tinygl_draw_point (prev_pos, 0);
 }
 
-
+/* Update the  paddle positions to the relevant directions */
 void point_update (tinygl_point_t pos1, tinygl_point_t pos2, tinygl_point_t pos3, int direction) {
 	
 
@@ -70,8 +73,7 @@ void point_update (tinygl_point_t pos1, tinygl_point_t pos2, tinygl_point_t pos3
 	}
 }
 
-
-
+/* Move paddle one to the left by updating the positions by 1 */
 void north_paddle(int direction) {
 	
 	paddle.pos.y--;
@@ -82,6 +84,7 @@ void north_paddle(int direction) {
 	
 }
 
+/* Move paddle one to the right by updating the positions by 1 */
 void south_paddle(int direction) {
 	
 	paddle.pos.y++;
@@ -92,24 +95,24 @@ void south_paddle(int direction) {
 	
 }
 
-
+/* Check to make sure the paddle is within the matrix length before moving left */
 int check_paddle_north(void) {
 	
 	
 	if (paddle.pos_3.y >= TINYGL_HEIGHT/2) {
 		return 1;
-	}else {
+	} else {
 		return 0;
 	}
 }
 	
-
+/* Check to make sure the paddle is within the matrix length before moving right */
 int check_paddle_south(void) {
 	
 	
 	if (paddle.pos.y <= TINYGL_HEIGHT/2) {
 		return 1;
-	}else {
+	} else {
 		return 0;
 	}
 }
