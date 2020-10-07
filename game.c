35421a5c7c0168aab198b3ef82629652fc2e9646
Rachel <ColourfulDemon@Rachels-MacBook-Air.local>
2020-10-07 19:20:47 +1300

    Enhanced style

--- game.c
+++ game.c
@@ -7,9 +7,7 @@
  */
 
 #include <stdlib.h>
-
 #include "paddle.h"
-
 #include "pio.h"
 #include "system.h"
 #include "display.h"
@@ -22,37 +20,40 @@
 #include "timer.h"
 
 
-
-
 /* Define polling rate in Hz.  */
 #define LOOP_RATE 900
-
 
 
 int main (void){
 	
 	int direction = 0;
+    
+    /* Initialising the system, navswitch and paddle modules */
 	system_init ();
 	navswitch_init ();
-	
     paddle_initial();
     
+    /* Initialising tinygl and relevant tinygl functions */
     tinygl_init (LOOP_RATE);
     tinygl_font_set (&font3x5_1);
     
+    /* Set the initial parameters */
     paddle_initial_points();
     
     while (1)
     {
 
+        /* Check if the switch has been pressed */
         navswitch_update ();
 
+        /* If switch pushed up, move the paddle to the right */
         if (navswitch_push_event_p (NAVSWITCH_NORTH) && check_paddle_north() == 1)
         {
 			north_paddle(direction);
 
         }
         
+        /* If switch pushed down, move the paddle to the left */
         if (navswitch_push_event_p (NAVSWITCH_SOUTH) && check_paddle_south() == 1)
         {
             south_paddle(direction);
