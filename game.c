65b5393b1562663c0501385e3c9e58bcb127d5e9
Rachel <ColourfulDemon@Rachels-MacBook-Air.local>
2020-10-14 15:14:24 +1300

    Yayyyyy

--- game.c
+++ game.c
@@ -108,8 +108,8 @@ int main (void)
 						col = recieved.col_s;
 						state = 1;
 						
-						
 						ball = ball_set_high(row, col, ball);
+                        
 					} else if (data_received == 30){
                         
 						ball = ball_set_low(row, col, ball);
