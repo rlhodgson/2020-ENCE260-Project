659e5b02c677cb53ce7d5f4932d84df24b7c799a
Rachel <ColourfulDemon@Rachels-MacBook-Air.local>
2020-10-14 14:13:44 +1300

    Tidy up

--- game.c
+++ game.c
@@ -1,13 +1,12 @@
 /**
     @file game.c
     @author Matthew Bond, Rachel Hodgson
-    @date 07 October 2020
+    @date 15 October 2020
     @brief Main game program
  
  */
 
 #include <stdlib.h>
-
 #include "paddle.h"
 #include "pio.h"
 #include "system.h"
@@ -28,45 +27,49 @@
 
 #define MESSAGE_RATE 10
 
+/* Struct definition for state variables */
 typedef struct Pos_state_s {
+    
 	int row_s;
 	int col_s;
 } Pos_state;
 
-
+/* Print game over when game is over */
 void game_over(void) {
+    
 	tinygl_text("Hello world\0");
 	tinygl_update ();
 }
 
-
+/* Main program loop */
 int main (void)
 {
 	
 	uint16_t ball_tick = 0;
     uint16_t nav_tick = 0;
     
-
+    // initialisers for ball movement
     int row = 0;
     int col = 1;
     int rowinc = 1;
     int colinc = 1;
     
-    
     system_init ();
-    //led_init();
 
     pacer_init (LOOP_RATE);
     
+    // Initialiser for direction of paddle movement, check for paddle hit or miss and the state of the game
     int direction = 0;
     int check = 1;
     int state = 0;
     
     
-	/* Initialising the system, navswitch and paddle modules */
+	// Initialising the system, navswitch and paddle modules
     paddle_initial();
+    paddle_initial_points();
+    ir_uart_init();
     
-	/* Initialising tinygl and relevant tinygl functions */
+	// Initialising tinygl and relevant tinygl functions
     tinygl_init (LOOP_RATE);
     tinygl_font_set (&font3x5_1);
    
@@ -76,29 +79,21 @@ int main (void)
 	tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
     tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
 
-    paddle_initial_points();
-    
-    ir_uart_init();
-    
-    
+    // Variable to received IR communications
     Pos_state recieved = {0, 0};
 
-
+    // Main loop
     while (1)
     {
-		
-		
 		pacer_wait ();
 		
 		ball_tick++;
 		
-			
 		if (ball_tick >= LOOP_RATE / BALL_RATE) {
 				
 			ball_tick = 0;
 			
-			
-			
+            //check game state
 			if (state == 0) {
 				if (ir_uart_read_ready_p()!= 0) {
 					
@@ -114,17 +109,22 @@ int main (void)
 						
 						ball = ball_set_high(row, col, ball);
 					} else {
+                        
 						ball = ball_set_low(row, col, ball);
+                        
+                        // Turn on blue LED for winner
                         PORTC |= (1 << 2);
+                        
                         //tinygl_text('W\0');
 						break;
 					
                     }
-			}
-		}
+                }
+            }
 				
 			if (state == 1) {
 				
+                // If the ball is at the top of the board
 				if (col == 0) {
 					
 					ball = ball_set_low(row, col, ball);
@@ -137,52 +137,42 @@ int main (void)
 				}
 				
 				else {
-				
-					
-			
+                    
 					ball = ball_set_low(row, col, ball);
 					
-
+                    // If it is at the bottom, check if it hit the paddle
 					if (col == 4) {
 						
 						check = check_ball_paddle(ball);
 					}
 
-						
+                    // If it hit the paddle then continue as normal
 					if (check == 1) {
-						
-						// move ball
-					
+											
 						col += colinc;
 						row += rowinc;
 						
 						// check ball is within the right place
-
 						if (row > 6 || row < 0)
 						{
-							// in future we woukd start the other screen if the row was < 0
 							row -= rowinc * 2;
 							rowinc = -rowinc;
 						}
 
 						if (col > 4 || col < 0)
 						{
-							// check if there has been a collision between ball and paddle, if so then continue
 							col -= colinc * 2;
 							colinc = -colinc;
 						}
-					
 				
 						ball = ball_set_high(row, col, ball);
 						
 						
 					} else {
 						ir_uart_putc(30);
-
-						
+                        
 						ball = ball_set_low(row, col, ball);
 						
-
 						break;
 
 					}
@@ -190,28 +180,27 @@ int main (void)
 			}
 		} 
 		
-			
-			
 		nav_tick++;
         if (nav_tick >= LOOP_RATE / NAV_LOOP_RATE) {
 			
 			nav_tick = 0;
 			navswitch_update ();
 			
+            // Check if navswitch was pushed
 			if (navswitch_push_event_p (NAVSWITCH_PUSH))
 			{
 				state = 1;
 
 			}
 			
-			/* If switch pushed down, move the paddle to the right */
+			// If switch pushed down, move the paddle to the right
 			if (navswitch_push_event_p (NAVSWITCH_NORTH) && check_paddle_north() == 1)
 			{
 				north_paddle(direction);
 
 			}
 			
-			/* If switch pushed down, move the paddle to the left */
+			// If switch pushed down, move the paddle to the left
 			if (navswitch_push_event_p (NAVSWITCH_SOUTH) && check_paddle_south() == 1)
 			{
 				south_paddle(direction);
@@ -222,41 +211,5 @@ int main (void)
 		
 		tinygl_update ();
 			
-			
 		}
-		game_over();
-		tinygl_update ();
-		
-		
-		
-
-		
-		
 }
-
-        
-
-        
-		
-		
-		
-		
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-    
-
-    
-
-
-
-
